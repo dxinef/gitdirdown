0486fd8797022ee73438be279ae7ff5890f44007
dxinef <7739224@qq.com>
2018-04-11 11:36:54 +0800

    first commit

---
+++ bin/cli.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const cmd = require("commander");
+const gitDownload = require("../index");
+const pkg = require("../package.json");
+
+cmd.version(pkg.version, "-v, --version")
+    .usage('gitdirdown <url> [options]')
+    .option("-u, --url <url>", "git项目的url，必填")
+    .option("-s, --save <path>", "下载文件保存的目录，默认为当前目录")
+    .option("-p, --path <path>", "子级目录，默认为空")
+    .option("-b, --branch <branch>", "项目分支，默认为master")
+    .parse(process.argv);
+
+var uri = cmd.url;
+if(!uri)  throw("error: need argument --url");
+
+var u = new (require("url").URL)(uri);
+
+var site = "";
+if(u.host=="github.com") site = "github";
+else if(u.host=="gitee.com") site = "gitee";
+else throw ("error: wrong url.");
+
+var m = u.pathname.match(/^\/([\w\-\.]+)\/([\w\-\.]+)(\/tree\/([\w\-\.]+)\/([\w\-\.\/]+))?/);
+
+if(!m) throw ("error: wrong url");
+
+// gitDownload
+console.log("准备就绪: " + uri);
+
+gitDownload({
+    site: site,
+    owner: m[1],
+    repo: m[2],
+    branch: cmd.branch || m[4] || "",
+    path: (cmd.path || m[5] || "").replace(/\/$/, ""),
+    save: cmd.save || "",
+    onComplete: function() {
+        console.log("任务完成!");
+    }
+});
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+gitdirdown
+====
+
+一个用来下载git下文件的小工具。
+
+- 支持下载一个git仓库的所有文件
+- 支持下载一个git仓库的某个目录
+- 下载时可以指定branch
+
+目前支持github/gitee
+
+```
+gitdirdown -u https://github.com/dxinef/gitdirdown -s tempdownload
+```
